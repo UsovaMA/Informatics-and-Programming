e41393329dbdeb6b48c4b0117162eb1473ece839
Марина Усова <oppabang@outlook.com>
2021-09-10 13:21:47 +0300

    добавлена поддержка русского языка

--- TasksInTerminalClass/TerminalClassSolution/CalculateOfAreas/main.c
+++ TasksInTerminalClass/TerminalClassSolution/CalculateOfAreas/main.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
+#include <locale.h>
 
 int main() {
+  setlocale(LC_ALL, "russian");
+
   // init data
   int a, h;
   float S;
 
-  printf("Input two parameters of triangle (through space): ");
+  printf("¬ведите два параметра треуольника (через пробел): ");
   scanf_s("%d %d", &a, &h);   // & - адрес
 
   // action
   S = 0.5 * a * h;
 
   // result
-  printf("S = %.2f", S);
+  printf("ѕлощадь: S = %.2f", S);
 
   return 0;
 }
