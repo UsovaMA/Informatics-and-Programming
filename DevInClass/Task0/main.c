53e36f4b986117abb5ce0a724c0361c39cd687d3
W-Snack <ase3938@gmail.com>
2021-09-17 10:09:40 +0300

    add solution for HW-0 (Vzaimnoe raspolozhenie 2 okrujnosti)

---
+++ DevInClass/Task0/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h> 
+#include <math.h>
+//Opredelit', kakovo vzaimnoe raspolozhenie dvuh okruzhnostej s zadannymi koordinatami centrov i radiusami.
+
+int main() {
+	int x1, x2, y1, y2, rad1, rad2;
+	float dist;
+	//input
+	printf("Programma opredelenia vzaimnogo raspolojenia dvuh okrujnostei\n\n");
+	printf("Vvedite parametri okrujnostei: \n koordinati centra 1 okrujnosti and radius (x  y radius (cherez probel)\n");
+	scanf_s("%d %d %d", &x1, &y1, &rad1);
+	printf("Vvedite parametri okrujnostei: \n koordinati centra 2 okrujnosti and radius (x  y radius (cherez probel)\n");
+	scanf_s("%d %d %d", &x2, &y2, &rad2);
+	// solution
+	dist = (float)(sqrt(pow((x2 - x1),2) + pow((y2 - y1),2)));
+	if (dist > (rad1 + rad2)) {
+		printf("Kazhdaya iz okruzhnostej lezhit vne drugoj.");
+	}
+	else {
+		if (dist == rad1 + rad2) {
+			printf("Vneshnee kasanie dvuh okruzhnostej");
+		}
+		else {
+			if (dist == rad1 - rad2 ) {
+				printf("Vnutrennee kasanie dvuh okruzhnostej");
+
+			}
+			else {
+				if ((rad1 - rad2 ) < dist < (rad1 + rad2)) {
+					printf("Okruzhnosti peresekayutsya v dvuh tochkah");
+
+				}
+				else {
+					if (dist < rad1 - rad2) {
+						printf("2 Okrujnost lejit vnutri 1");
+
+					}
+					else {
+						printf("1 okrujnost lejit vnutr 2");
+
+					}
+				}
+			}
+		}
+	}
+	
+	return 0;
+}
