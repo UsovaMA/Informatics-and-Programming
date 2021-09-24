dfde1b8bdd36ad71500e2f71fa31ec8f7e3a016f
Skotin Alexandr <sascha.scotin2011@list.ru>
2021-09-24 10:19:34 +0300

    Fix location folders

--- TasksInTerminalClass/CoinWitcher/Main.c
+++ TasksInTerminalClass/CoinWitcher/Main.c
@@ -6,7 +6,7 @@ int main()
 	setlocale(LC_ALL, "rus");
 
 	int coin; // Цена услуги
-	int k = 0;// Кол-во монет
+	int k1 = 0,k5 = 0,k10 = 0,k25 = 0;// Кол-во монет
 	printf("Цена услуги: ");
 	scanf_s("%d", &coin);
 	while (coin > 0)
@@ -14,23 +14,23 @@ int main()
 		if (coin - 25 >= 0)
 		{
 			coin -= 25;
-			k++;
+			k25++;
 		}
 		else if (coin - 10 >= 0)
 		{
 			coin -= 10;
-			k++;
+			k10++;
 		}
 		else if (coin - 5 >= 0)
 		{
 			coin -= 5;
-			k++;
+			k5++;
 		}
-		else if (coin - 1 >= 0)
+		else
 		{
 			coin -= 1;
-			k++;
+			k1++;
 		}
 	}
-	printf("\nМинимальное количество монет: %d", k);
+	printf("25 - %dшт.\n10 - %dшт.\n5 - %dшт.\n1 - %dшт.\n", k25,k10,k5,k1);
 }
