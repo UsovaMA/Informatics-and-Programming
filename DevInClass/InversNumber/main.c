9d67edac3d0dc98a4d7b0fc1978b24bb2fd214a7
W-Snack <ase3938@gmail.com>
2021-10-01 10:44:09 +0300

    Add solution for inverse number or sum of digits

--- DevInClass/InversNumber/main.c
+++ DevInClass/InversNumber/main.c
@@ -6,8 +6,8 @@ int main()
 {
 	int num, method, IsError, sum;
 	sum = IsError = 0; // flag
-	printf("This Programm for finding inverse number or sum\n");
-	printf("If you want to find inverse number enter 1\nif you want to find sum enter 2\nFor exit print 0\n");
+	printf("This Programm for finding inverse number or sum of digits\n");
+	printf("If you want to find inverse number enter 1\nif you want to find sum of digits enter 2\nFor exit print 0\n");
 	while (1) {
 		printf("Input method: ");
 		scanf_s("%d", &method);
@@ -24,14 +24,16 @@ int main()
 			if (!num) continue;
 			printf("Invers number: ");
 			while (num) {
-				printf("%d", num % 10);
+				if (num % 10) {
+					printf("%d", num % 10);
+				}
 				num /= 10;
 			}
 			printf("\n");
 		}
 		else if (method == 2) {
 			do {
-				if (IsError) printf("Error. Number isnt positive. Try again");
+				if (IsError) printf("Error. Number isnt positive. Try again\n");
 				printf("Input positive number\nFor exit print 0\n");
 				scanf_s("%d", &num);
 				IsError = 1;
@@ -42,7 +44,7 @@ int main()
 				sum = sum + num % 10;
 				num /= 10;
 			}
-			printf("Sum = %d\n", sum);
+			printf("Sum of digits = %d\n", sum);
 		}
 		else if (method == 0) break;
 	}
