40b8729b83ab01460462634720009692c21350f5
NikitinKU <clownzzzz@mail.ru>
2021-10-01 10:29:37 +0300

    update coding style

--- HW/HWs/Task0/main.c
+++ HW/HWs/Task0/main.c
@@ -2,8 +2,7 @@
 #include <math.h>
 #include <locale.h>
 
-int main()
-{
+int main() {
 	printf("Task0 with circles");
 	//init
 	float x1, x2, y1, y2, r1, r2;					// x1, x2, и y1, y2 - координаты; r1 и r2 - радиусы
@@ -23,23 +22,25 @@ int main()
 
 	if (d == r1 + r2) {
 		printf("Kasautsa snaruji");
-	}
-	else
+	} else {
 		if (d < r1 + r2) {
 			if ((r1 == r2) && (x1 == x2) && (y1 == y2)) {	//&& = AND
 				printf("Sovpadaet");
-			}
-			else if (d + r1 < r2 || d + r2 < r1) {			// || = OR
-					printf("Odna vnuutri drugoy");
-			} else if (d + r1 == r2 || d + r2 == r1) {
-				    printf("Kasautsa odna vnutri drugoy");
 			} else {
-				printf("Peresekautsa");
+				if (d + r1 < r2 || d + r2 < r1) {			// || = OR
+					printf("Odna vnuutri drugoy");
+				}
+				else {
+					if (d + r1 == r2 || d + r2 == r1) {
+						printf("Kasautsa odna vnutri drugoy");
+					} else {
+						printf("Peresekautsa");
+					}
+				}
 			}
+		} else {
+			printf("Ne kasautsa");
 		}
-		else {
-				printf("Ne kasautsa");
-	    } 
-
+	}
 	return 0;
 }
