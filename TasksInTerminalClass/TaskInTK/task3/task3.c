243dbd7c49bbbc9bddef577db5c929020fc1a04e
Anton Savchuk <savchuk.anton.s@gmail.com>
2021-10-06 20:41:10 +0300

    added task3 in Terminal Class

--- TasksInTerminalClass/TaskInTK/task3/task3.c
+++ TasksInTerminalClass/TaskInTK/task3/task3.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
 #include <math.h>
+#ifndef __cplusplus
+#define bool int
+#define true 1
+#define false 0
+#endif
 #define _CRT_SECURE_NO_WARNINGS
 
-int main()/*Start programm */ {
-	int number = 0, working = 1;
-	while (working){
+int main() {
+	int num, inverse_num = 0;
 
-		int error = 0;
-		do {
-			if (error)
-				printf("Wrong range of number, please, try again!\n");
-			
-			printf("Enter number > 0: ");
-			scanf_s("%d", &number);
-			if (number == 0)
-				working = 0;
+	while (true)
+	{
+		printf("Input the number: ");
+		scanf_s("%d", &num);
+		if (num > 0) 
 			break;
-
-		} while (error);
+		else
+			printf("Wrong range, choose another number!\n");
 	}
+
+	while (num != 0) {
+		
+		inverse_num *= 10;
+		inverse_num += num % 10;
+		num /= 10;
+	}
+
+	printf("Inverse number equals: %d", inverse_num);
+
 }
