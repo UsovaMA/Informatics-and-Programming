7933dde90e432b6a641fe568185428f023c5ec81
Burdyuja Alexandr <leomessi-2003-23@mail.ru>
2021-10-12 18:49:31 +0300

    changed task4

--- TasksInTerminalClass/TK/task4/task4.c
+++ TasksInTerminalClass/TK/task4/task4.c
@@ -1,17 +1,27 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-int main()
-{
-    int a[5];
-    int i;
-    for (i = 0; i < 5; i++)
-    {
-        printf("a[%d] = ", i);
-        scanf("%d", &a[i]);
-    }
-    for (i = 5; i <= 5; i--) 
-    {
-        printf("%d ", a[i]);
-    }
-    return 0;
+#include <malloc.h>
+#include <math.h>
+
+
+
+int main() {
+	int* m;
+	int n;
+
+	printf("vvedite razmer massiva ");
+	scanf_s("%d", &n);
+
+	m = (int*)malloc(n * sizeof(int));
+
+	for (int i = 0; i < n; i++) {
+		printf("vvedite %d element: ", i + 1);
+		scanf_s("%d", &m[i]);
+	}
+
+	for (int i = n - 1; i > -1; i--) {
+
+		printf(" %d\n", m[i]);
+
+	}
+	return 0;
 }
