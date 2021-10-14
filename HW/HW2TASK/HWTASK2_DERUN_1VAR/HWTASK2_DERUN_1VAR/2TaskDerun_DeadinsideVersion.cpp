fc5c0f61d179b82f287996833c9fea115c92e223
AndrewStiks <slipfis@gmail.com>
2021-10-14 22:06:18 +0300

    coment

--- HW/HW2TASK/HWTASK2_DERUN_1VAR/HWTASK2_DERUN_1VAR/2TaskDerun_DeadinsideVersion.cpp
+++ HW/HW2TASK/HWTASK2_DERUN_1VAR/HWTASK2_DERUN_1VAR/2TaskDerun_DeadinsideVersion.cpp
@@ -7,14 +7,13 @@ int main()
     system("color 0B");
     setlocale(LC_CTYPE, "rus");
     srand((unsigned int)time(NULL));
-    int rmin = 0, rmax = 1000;
-    int variable_glavn = 0, question = ((rmax - rmin) * ((double)rand()) / RAND_MAX + rmin), user_answer = 0, i = 1000, Otvet = 0;
+    int rmin = 0, rmax = 1000, rmid = rmin + ((rmax - rmin) / 2);
+    int variable_glavn = 0, variable_case_two, question = ((rmax - rmin) * ((double)rand()) / RAND_MAX + rmin), user_answer = 0;
     cout << "Программа загадает или ты ей" << endl; //  программа загадывает,  я загадываю программе
     do
     {
         cout << "1. вариант первый\n";
         cout << "2. вариант второй\n";
-        cout << "3. Интересно,  зачем  оно здесь?\n";
         cout << "0. SUDO HALT\n";
         cin >> variable_glavn;
         switch (variable_glavn)
@@ -31,7 +30,7 @@ int main()
                 }
                 else if (user_answer < question)
                 {
-                    cout << "Джиброни больше чем твоя самооценка " << endl;
+                    cout << "Джиброни больше, чем твоя самооценка " << endl;
                 }
             }
             cout << "Как истинный данжн мастер ты смог победить меня";
@@ -43,36 +42,52 @@ int main()
             system("CLS");
             cout << "Загадай мне число джиброни босс " << endl;
             cin >> question;
-
+            do
+            {
+                system("CLS");
+                cout << "Загаданное число больше или меньше, чем " << rmid << endl;
+                cout << "1. Больше\n";
+                cout << "2. Меньше\n";
+                cout << "9. ПОБЕДА\n";
+                cout << "0. SUDO HALT\n";
+                cin >> variable_case_two;
+                switch (variable_case_two)
+                {
+                case 1: //больше
+                    rmin = rmid;
+                    rmid = rmin + round(double((rmax - rmin) / 2));
+                    break;
+                case 2: //меньше
+                    rmax = rmid;
+                    rmid = rmin + round(double((rmax - rmin) / 2));
+                    break;
+                case 9: //победа-победа, вместо обеда
+                    system("CLS");
+                    cout << "Я в себе и не сомневался, ведь я же компьютер! (ткни любую кнопку)";
+                    getchar();
+                    getchar();
+                    system("CLS");
+                    break;
+                case 0: // экстренный рестарт
+                    return 0;
+                default:
+                    system("CLS");
+                    cout << "Куда жмёшь, слейв?";
+                    getchar();
+                    getchar();
+                    system("CLS");
+                    break;
+                }
+            } while (variable_case_two != 0);
             getchar();
             getchar();
             system("CLS");
             break;
         case 0:
             return 0;
-        case 3:
-            system("CLS");
-            cout << "3 от тысячи...хм это получается\n";
-            cout << "1000-7 одно и тоже...сколько будет 1000-7...?\n";
-            cin >> Otvet;
-            if (Otvet == 993)
-            {
-                while (i > 7)
-                {
-                    i = i - 7;
-                    cout << i << " - 7\n";
-                    Sleep(10);
-                }
-                cout << " Dead Inside with deep deep dark fantasy";
-
-                getchar();
-                getchar();
-            }
-            system("CLS");
-            break;
         default:
             system("CLS");
-            cout << "Куда жмёшь слейв?";
+            cout << "Куда жмёшь, слейв?";
             getchar();
             getchar();
             system("CLS");
