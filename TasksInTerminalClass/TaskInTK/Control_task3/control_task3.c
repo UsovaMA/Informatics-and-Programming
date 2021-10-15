ea57033de12c9be9ed9f01f3d4bb95333570d315
Anton Savchuk <savchuk.anton.s@gmail.com>
2021-10-15 10:39:28 +0300

    added control task3 without soution

---
+++ TasksInTerminalClass/TaskInTK/Control_task3/control_task3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <math.h>
+
+int main() {
+	int n, count;
+	int* list;
+	int* indexes;
+	printf("Input count of citizens: ");
+	scanf_s("%d", &n);
+
+	list = (int)malloc(n * sizeof(int));
+
+	for (int i = 0; i < n; i++) {
+		int tmp;
+		printf("%d Friends ", i + 1);
+		scanf_s("%d", &tmp);
+		list[i] = tmp;
+	}
+
+	for (int i = 0; i < n; i++) {
+		printf(" %d friends = %d\n", i + 1, list[i]);
+	}
+
+	int* friends;
+
+
+
+	free(list);
+}
