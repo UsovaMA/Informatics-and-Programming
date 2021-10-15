55b092c50602a61a1e4068e0dc34fd9ca1d02174
Burdyuja Alexandr <leomessi-2003-23@mail.ru>
2021-10-15 16:30:45 +0300

    changed task4

--- TasksInTerminalClass/TK/task4/task4.c
+++ TasksInTerminalClass/TK/task4/task4.c
@@ -5,23 +5,24 @@
 
 
 int main() {
-	int* m;
-	int n;
+	int* m, m1;
+	int n, i, i1=0;
 
 	printf("vvedite razmer massiva ");
-	scanf_s("%d", &n);
+	scanf_s("%d\n", &n);
 
 	m = (int*)malloc(n * sizeof(int));
+	m1 = (int*)malloc(n * sizeof(int));
 
-	for (int i = 0; i < n; i++) {
-		printf("vvedite %d element: ", i + 1);
+	for (i = 0; i < n; i++) {
+		printf(" m[%d] = ", i + 1);
 		scanf_s("%d", &m[i]);
 	}
-
-	for (int i = n - 1; i > -1; i--) {
-
-		printf(" %d\n", m[i]);
-
+	for (i = n - 1; i >= 0; i--, i1++) {
+		m1[i1] = m[i];
+	}
+	for (i1 = 0; i1 < n; i1++) {
+		printf("%d ", m1[i1]);
 	}
 	return 0;
 }
