1df20ed49c4befd3b44201b0099e12302e3f0e50
UsovaMA <oppabang@mail.ru>
2021-10-29 09:29:15 +0300

    добавлена программа со сравнением сортировок

---
+++ Materials/main.c
@@ -0,0 +1,171 @@
+// Copyright 2021 Marina Usova
+
+/*
+Постановка задачи.
+
+Написать программу для сравнения двух сортировок.
+
+*/
+
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include <time.h>
+
+int generateRandNumber(int max, int min) {
+  int value = (max - min) * ((float)rand() / RAND_MAX) + min;
+  return value;
+}
+
+void swap(int* mass, int indx1, int indx2) {
+  int temp = mass[indx1];
+  mass[indx1] = mass[indx2];
+  mass[indx2] = temp;
+}
+
+void printArray(int* mass, int n) {
+  for (int i = 0; i < n; i++) {
+    printf("%d ", mass[i]);
+  }
+}
+
+void bubbleSort(int* mass, int n, int* countOfComp, int* countOfSwap) {
+  int temp;
+  (*countOfComp) = 0;
+  (*countOfSwap) = 0;
+  // сортировка по возрастанию с запоминанием индексов
+  for (int j = 0; j < n; j++) {
+    for (int i = 0; i < n; i++) {
+      // если встретились значения в неправильном порядке - меняем их местами
+      // меняем местами и их индексы, чтобы получить порядок элементов
+      if (mass[i] < mass[i + 1]) {
+        swap(mass, i, i + 1);
+        (*countOfSwap) += 1;
+      }
+      (*countOfComp) += 1;
+    }
+  }
+}
+
+void bubbleSortWithFlag(int* mass, int n, int* countOfComp, int* countOfSwap) {
+  int temp;
+  (*countOfComp) = 0;
+  (*countOfSwap) = 0;
+  // сортировка по возрастанию с запоминанием индексов
+  for (int j = 0; j < n; j++) {
+    int all_right = 1;
+    for (int i = 0; i < n; i++) {
+      // если встретились значения в неправильном порядке - меняем их местами
+      // меняем местами и их индексы, чтобы получить порядок элементов
+      if (mass[i] < mass[i + 1]) {
+        swap(mass, i, i + 1);
+        all_right = 0;
+        (*countOfSwap) += 1;
+      }
+      (*countOfComp) += 1;
+    }
+    // если ни одной перестановки на итерации не сделали - завершаем
+    if (all_right) break;
+  }
+}
+
+void countingSort(int* mass, int n, int min, int max, int* countOfComp, int* countOfSwap) {
+  int* count = NULL;
+  int size = max - min + 1;                          // -5 5 - 0 10 = 11 el
+  
+  count = (int*)malloc(size * sizeof(int));
+
+  (*countOfComp) = 0;
+  (*countOfSwap) = 0;
+
+  for (int i = 0; i < size; i++) {
+    count[i] = 0;
+  }
+  for (int i = 0; i < n; i++) {
+    count[mass[i] - min] += 1;
+  }
+  int ind = 0;
+  //for (int i = size - 1; i >= 0; i--) {
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < count[size - i - 1]; j++) {
+      mass[ind] = size - i - 1;
+      ind += 1;
+      (*countOfSwap) += 1;
+    }
+    (*countOfComp) += 1;
+  }
+}
+
+
+int main() {
+  int* mass = NULL, * copy_1 = NULL, * copy_2 = NULL, * copy_3 = NULL;
+  int n = 0, min = 0, max = 0;
+  int countOfComp = 0, countOfSwap = 0;
+
+  printf("Input array size: \n> ");
+  scanf_s("%d", &n);
+
+  printf("Input min and max value of random (between space): \n> ");
+  scanf_s("%d %d", &min, &max);
+
+  mass = (int*)malloc(n * sizeof(int));
+  copy_1 = (int*)malloc(n * sizeof(int));
+  copy_2 = (int*)malloc(n * sizeof(int));
+  copy_3 = (int*)malloc(n * sizeof(int));
+
+  for (int i = 0; i < n; i++) {
+    mass[i] = generateRandNumber(max, min);
+    copy_1[i] = mass[i]; copy_2[i] = mass[i]; copy_3[i] = mass[i];
+  }
+
+  if (n < 21) {
+    printf("Source array: ");
+    printArray(mass, n);
+  }
+
+  /* Sort with bubble sorting */
+
+  printf("\nStart sort with bubble sort...\n");
+
+  int time = clock();
+  bubbleSort(copy_1, n, &countOfComp, &countOfSwap);
+  time = clock() - time;
+
+  if (n < 21) {
+    printf("Sort array: ");
+    printArray(copy_1, n);
+  }
+  printf("\nTime of work: %d ms\nCount of comparisons: %d\nCount of swap: %d\n", time, countOfComp, countOfSwap);
+
+  /* Sort with bubble sorting whith flag for stop */
+
+  printf("\nStart sort with bubble sort with stop condition...\n");
+
+  time = clock();
+  //switch (sort) {
+  bubbleSortWithFlag(copy_2, n, &countOfComp, &countOfSwap);
+  //}
+  time = clock() - time;
+
+  if (n < 21) {
+    printf("Sort array: ");
+    printArray(copy_2, n);
+  }
+  printf("\nTime of work: %d ms\nCount of comparisons: %d\nCount of swap: %d\n", time, countOfComp, countOfSwap);
+
+  /* Countion sort */
+
+  printf("\nStart sort with bubble sort with stop condition...\n");
+
+  time = clock();
+  countingSort(copy_3, n, min, max, &countOfComp, &countOfSwap);
+  time = clock() - time;
+
+  if (n < 21) {
+    printf("Sort array: ");
+    printArray(copy_3, n);
+  }
+  printf("\nTime of work: %d ms\nCount of comparisons: %d\nCount of swap: %d\n", time, countOfComp, countOfSwap);
+
+  return 0;
+}
