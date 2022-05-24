1762ab1f7f52c735ec00e527d68ce37ea506e757
AndrewStiks <slipfis@gmail.com>
2022-05-24 22:02:00 +0300

    learn oop

---
+++ OOPLEARN/BASIC2/BASIC2/BASIC2.cpp
@@ -0,0 +1,58 @@
+﻿//#include <iostream>
+//using namespace std;
+//
+//class human {
+//
+//public:
+//	string name;
+//	int age;
+//	float weight;
+//
+//	human() { name = " "; }
+//	human(string name) { this->name = name; } // делигируем присвоение имени 
+//	human(string name, int age) : human(name) { this->age = age; } // делигирующий конструктов  
+//	human(string name, int age, float weight) : human(name,age) { this->weight = weight; }
+//};
+//int main()
+//{
+//setlocale(LC_ALL, "rus");
+//
+//human h(" alex ",25,98);
+//cout << "imya: " << h.name << "\nvozrast: " << h.age << "\nves: " << h.weight << endl;
+//};
+//
+//
+#include <iostream>
+using namespace std;
+
+class basic {
+public:
+	basic(const basic& other) //конструктор копир начало...ссылаемся на озер 
+	{
+		cout << "constructor viavan" << this << endl;
+	
+	this->x = other.x;
+	this->y = other.y; //конструктор копир конец
+}
+	basic(int px, int py): x(px), y(py){//инициализация с присвоением сразу 
+		cout << "const s 2 element" << this << endl;
+		
+	}
+	
+	~basic() {
+		cout << "destructor vizvan" << this << endl; 
+	}
+
+
+private:
+	int x, y;
+
+public:
+	void setcoord(int x, int y) { this->x = x;	this->y = y; }
+
+};
+	int main()
+	{
+		setlocale(LC_ALL,"rus");
+		basic yays(2,9);
+	};
---
+++ OOPLEARN/TASK3/TASK3/TASK3.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+class Children {
+private:
+	char name[20];
+	char famil[20];
+	double age;
+public:
+	void showdata();
+	void getdata();
+};
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	Children Child1;
+	Children Child2;
+	Child1.showdata();
+	Child1.getdata();
+	return 0;
+}
+//определеяем методы класса 
+void Children::showdata()
+{
+	cout << "Name:";
+	cin.getline(name, 20);
+	cout << "Famil:";
+	cin.getline(famil, 20);
+	cout << " Age: ";
+	cin >> age;
+	cin.get();
+}
+void Children::getdata()
+{
+	cout << name << "  " << famil << "  " << age << " age;\n ";
+}
